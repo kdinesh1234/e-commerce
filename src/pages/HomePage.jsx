4621f820106126b7f724ae13ee1b8e2c243ad886
kdinesh1234 <158142344+kdinesh1234@users.noreply.github.com>
2024-05-30 11:00:36 +0530

    Add files via upload

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,304 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const HomePage = () => {
+  
+  return (
+    <Layout title={"Best offers"}>
+      <div className="container">
+      <h1>PRODUCTS </h1>
+      </div>
+      
+<div className="d-flex-1">
+<div className="row contactus">
+        <div className="row-md-6 ">
+          
+          <img
+           
+            src="/images/a1.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+            
+
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Vintage Bell</h5>
+    <p className="card-text">Price:3000</p>
+    <p>category:Alarm</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          
+
+          
+          
+          
+          </div>
+          </div>
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/a2.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Kadio Green</h5>
+    <p className="card-text">Price:1000</p>
+    <p>category:Alarm</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/a3.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:oveera</h5>
+    <p className="card-text">Price:2000</p>
+    <p>category:Alarm</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+          </div>
+<div className="d-flex-2">
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/l3.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Acnos</h5>
+    <p className="card-text">Price:300</p>
+    <p>category:women</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/l1.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Casio</h5>
+    <p className="card-text">Price:400</p>
+    <p>category:women</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/m1.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:brown metal watch</h5>
+    <p className="card-text">Price:400</p>
+    <p>category:Men</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          
+          </div>
+          </div>
+
+          <div className="d-flex-3">  
+          <div className="row contactus ">
+        <div className="column-md-6 ">
+          <img
+            src="/images/m2.png"
+            
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+            
+            
+            
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Metal Watch</h5>
+    <p className="card-text">Price:250</p>
+    <p>category:Men</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+              
+          
+         
+          
+          
+          
+          </div>
+          </div>
+          
+          <div className="row contactus ">
+        <div className="column-md-6 ">
+          <img
+            src="/images/s1.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:design wrtch</h5>
+    <p className="card-text">Price:360</p>
+    <p>category:women</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+          <div className="row contactus ">
+        <div className="column-md-6 ">
+          <img
+            src="/images/s2.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Golden clock</h5>
+    <p className="card-text">Price:600</p>
+    <p>category:Wall Clock</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+          </div>
+
+          <div className="d-flex-4">
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/w3.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Office Wall Clock</h5>
+    <p className="card-text">Price:300</p>
+    <p>category:Wall Clock</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          
+          </div>
+          </div>
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/w1.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Personal Clock</h5>
+    <p className="card-text">Price360:</p>
+    <p>category: Wall Clock</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/w2.png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Wooden Clock</h5>
+    <p className="card-text">Price:250</p>
+    <p>Category: Wall Clock</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+
+          </div>
+          <div className="d-flex-5">
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/e1 (1).png"
+            alt="contactus"
+            style={{width: "80% ", height:"40vh" }}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:boat Smart watch</h5>
+    <p className="card-text">Price:500</p>
+    <p>category:smart watches</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          <img
+            src="/images/e2 (1).png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+          />
+           <div className="card-body">
+    <h5 className="card-title">Name:Fastrack watch</h5>
+    <p className="card-text">Price:1795</p>
+    <p>category:Smart watches</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+          <div className="row contactus ">
+        <div className="row-md-6 ">
+          
+          <img
+            src="/images/e3 (1).png"
+            alt="contactus"
+            style={{ height:"40vh", width: "80% "}}
+            
+          />
+          
+           <div className="card-body">
+    <h5 className="card-title">Name:Smart Watch 2</h5>
+    <p className="card-text">Price:$2050</p>
+    <p>category:Smart watches</p>
+    
+    <a href="#" className="btn btn-primary">ADD TO CART</a>
+  </div>
+          </div>
+          </div>
+
+          </div>
+
+
+
+      
+      
+    </Layout>
+  );
+};
+
+export default HomePage;
